1273e1100af04b512d822d6c65a32176a1380f91
Denil Bouafia <denilb@MacBook-Pro-de-Denil.local>
2023-12-16 06:44:59 +0100

    modification 16

--- 2023/16-2.py
+++ 2023/16-2.py
@@ -19,40 +19,33 @@ for k in range(2 * n + 2 * m):
         Q = [(k-(n+m), m-1, 'L')]
     else:
         Q = [(0, k-(2*n+m), 'D')]
+        
     while Q:
         i, j, d = Q.pop()
         if not(0<=i<n and 0<=j<m) or nrj[i][j]["UDLR".find(d)]:
             continue
         nrj[i][j]["UDLR".find(d)] = 1
-        if grid[i][j] == '.':
-            di, dj = DIR(d)
-            Q.append((i+di,j+dj,d))
-        elif grid[i][j] == '/':
+        if grid[i][j] == '/':
             d = {'R':'U','U':'R','L':'D','D':'L'}[d]
-            di, dj = DIR(d)
-            Q.append((i+di,j+dj,d))
         elif grid[i][j] == '\\':
             d = {'R':'D','D':'R','L':'U','U':'L'}[d]
-            di, dj = DIR(d)
-            Q.append((i+di,j+dj,d))
         elif grid[i][j] == '|':
             if d in 'RL':
                 di, dj = DIR('U')
                 Q.append((i+di,j+dj,'U'))
                 di, dj = DIR('D')
                 Q.append((i+di,j+dj,'D'))
-            else:
-                di, dj = DIR(d)
-                Q.append((i+di,j+dj,d))
+                continue
         elif grid[i][j] == '-':
             if d in 'UD':
                 di, dj = DIR('R')
                 Q.append((i+di,j+dj,'R'))
                 di, dj = DIR('L')
                 Q.append((i+di,j+dj,'L'))
-            else:
-                di, dj = DIR(d)
-                Q.append((i+di,j+dj,d))
+                continue
+        di, dj = DIR(d)
+        Q.append((i+di,j+dj,d))
+
     cur = 0
     for i in range(n):
         for j in range(m):
