d9576b732f03fd4676537fdee863c0ea19d10919
denilblecodeur <58654735+denilblecodeur@users.noreply.github.com>
2024-12-22 09:20:56 +0100

    day22-2 better

--- 2024/day22-2.py
+++ 2024/day22-2.py
@@ -1,4 +1,5 @@
 import sys
+from collections import defaultdict
 
 def f(x):
     x = ((x*64)^x)%16777216
@@ -6,8 +7,7 @@ def f(x):
     x = ((x*2048)^x)%16777216
     return x
 
-sequences = []
-possibilities = set()
+sequences = defaultdict(int)
 for line in sys.stdin:
     seed = int(line.strip('\n'))
     ban = [seed%10]
@@ -16,18 +16,11 @@ for line in sys.stdin:
         seed = f(seed)
         ban.append(seed%10)
         change.append(ban[-2]-ban[-1])
-    sc = dict()
+    sc = set()
     for i in range(len(change)-4):
         seq = tuple(change[i:i+4])
-        if seq not in sc:
-            sc[seq] = ban[i+4]
-            possibilities.add(seq)
-    sequences.append(sc)
+        if seq in sc: continue
+        sc.add(seq)
+        sequences[seq] += ban[i+4]
 
-ans = 0
-for p in possibilities:
-    res = 0
-    for sc in sequences:
-        res += sc.get(p, 0)
-    ans = max(ans, res)
-print(ans)
+print(max(sequences.values()))
